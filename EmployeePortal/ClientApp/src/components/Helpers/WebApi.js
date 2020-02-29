016c94b9f055e23726534ebf29d15da3427a5986
Vinay Sharma <sharma.ricky@gmail.com>
2020-02-29 23:18:04 +1100

    UI and fetch redirect fixes.

--- EmployeePortal/ClientApp/src/components/Helpers/WebApi.js
+++ EmployeePortal/ClientApp/src/components/Helpers/WebApi.js
@@ -19,10 +19,10 @@ function WebApi(apiUrl, data, method = 'POST', auth = true) {
     if (method === 'POST' || method === 'PUT')
         requestInfo.body = data
 
-    return trackPromise(fetch('http://employee.service.com' + apiUrl, requestInfo).then(res => {
+    return trackPromise(fetch('http://employee.service.com' + apiUrl, requestInfo).then(res => {        
         if (res.ok) {
             return res.json();
-        } else {
+        } else {            
             var error = res.text();
             let errorStatus = res.statusText
             if (errorStatus.toUpperCase() === 'UNAUTHORIZED') {
@@ -34,6 +34,10 @@ function WebApi(apiUrl, data, method = 'POST', auth = true) {
     }).then(result => {
         return result;
     }, error => {
+        if (error.toString() === 'TypeError: Failed to fetch') {
+            alert('Session has expired.')
+            localStorage.removeItem('myToken')
+        }        
         return error;
     }))
 }
