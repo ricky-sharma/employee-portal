82c2ecb532660ca77dc82e415fa7b4898ef26516
Vinay Sharma <sharma.ricky@gmail.com>
2022-07-06 22:38:49 +1000

    Enable server logging in React js

---
+++ EmployeePortal/ClientApp/src/components/Helpers/Logger.ts
@@ -0,0 +1,26 @@
+﻿import { useEffect, useState } from 'react';
+import { Service } from './Service';
+import { Dictionary } from './Dictionary';
+import WebApi from './WebApi';
+
+export function Logger(log: Dictionary<string>) {
+    WebApi('/api/Logger', JSON.stringify(log)).then(r => true);
+}
+
+
+export function useLogService() {
+    const [result, setResult] = useState < Service < Dictionary < string > [] >> ({
+        status: 'loading'
+    });
+
+    useEffect(() => {
+        WebApi('/api/Logger', '','GET')
+            .then(response => response.json())
+            .then(response => setResult({ status: 'loaded', payload: response }))
+            .catch(error => setResult({ status: 'error', error }));
+    }, []);
+
+    return result;
+}
+
+export default { Logger, useLogService };
--- SQLDataEntity/DBFirstEntity.Context.cs
+++ SQLDataEntity/DBFirstEntity.Context.cs
@@ -27,11 +27,12 @@ namespace SQLDataEntity
     
         public virtual DbSet<tblDepartment> tblDepartments { get; set; }
         public virtual DbSet<tblEmployee> tblEmployees { get; set; }
-        public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
         public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
         public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
         public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
         public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
         public virtual DbSet<AspNetUserInfo> AspNetUserInfoes { get; set; }
+        public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
+        public virtual DbSet<tblLog> tblLogs { get; set; }
     }
 }
