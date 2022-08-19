c77aee77628c86b6b6858c803e683ab467e6784a
Vinay Sharma <sharma.ricky@gmail.com>
2022-08-19 13:51:26 +1000

    code fix

--- EmployeePortal/service-worker.js
+++ EmployeePortal/service-worker.js
@@ -13,10 +13,6 @@
 
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-importScripts(
-  "/precache-manifest.6b2ff517673808886d3271197afc6ab5.js"
-);
-
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
