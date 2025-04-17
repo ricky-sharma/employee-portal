824e70e1eaab1627dcf9c8e89602d50a2df20cc0
Vinay Sharma <sharma.ricky@gmail.com>
2025-04-17 23:03:55 +1000

    Bug-fixes-and-updates- Error Boundary fix

--- EmployeePortal/ClientApp/src/components/Helpers/ComponentWrapper.js
+++ EmployeePortal/ClientApp/src/components/Helpers/ComponentWrapper.js
@@ -2,10 +2,10 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 
 // This functional wrapper component gives class components access to `location` and 'navigate' via props
-const ComponentWrapper = ({ component: Component }) => {
+const ComponentWrapper = ({ component: Component, ...props }) => {
     const location = useLocation();
     const navigate = useNavigate();
-    return <Component location={location} navigate={navigate} />;
+    return <Component location={location} navigate={navigate} {...props} />;
 };
 
 export default ComponentWrapper;
